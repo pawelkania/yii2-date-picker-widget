6f9b9dc56fb057a9e3b5ba6057371f7d11ff8e8c
pawelkania <poczta@aquasite.pl>
2017-06-23 20:28:24 +0200

    Update DateRangePickerAsset.php

--- src/DateRangePickerAsset.php
+++ src/DateRangePickerAsset.php
@@ -18,7 +18,7 @@ use yii\web\AssetBundle;
  */
 class DateRangePickerAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/2amigos/yii2-date-picker-widget/src/assets';
+    public $sourcePath = '@vendor/pawelkania/yii2-date-picker-widget/src/assets';
 
     public $css = [
         'css/bootstrap-daterangepicker.css'
